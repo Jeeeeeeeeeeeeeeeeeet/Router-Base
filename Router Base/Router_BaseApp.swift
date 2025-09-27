46598da2f10328e14f4a06ebb39848b0f9007582
Jeeeeeeeeeeeeeeeeeet <148844906+Jeeeeeeeeeeeeeeeeeet@users.noreply.github.com>
2025-09-27 15:44:46 +0530

    Initial Commit

---
+++ Router Base/Router_BaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Router_BaseApp.swift
+//  Router Base
+//
+//  Created by Jeet Pardeshi on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Router_BaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
